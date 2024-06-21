ed025d784186d56bfd04cea88acbba50f76cd07a
karczuRF <oskar.karcz@rumblefish.pl>
2024-06-21 11:23:42 +0200

    ref: add ts scripts to register tapp

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
-import * as core from "@actions/core";
+import * as core from '@actions/core'
+import { addTappletToRegistry } from './register'
+import { addAndFormatCodeowners } from './codeowners/codeowners'
 
 /**
  * The main function for the action.
@@ -7,13 +9,15 @@ import * as core from "@actions/core";
 export async function run(): Promise<void> {
   try {
     // Add new tapplet to the registry
-    // await addTappletToRegistry(tappletCandidate)
-    // core.notice(`The ${tappletCandidate.displayName} tapplet added to registry`)
+    const registeredManifests = addTappletToRegistry()
+    core.notice(
+      `Registry updated: ${registeredManifests} tapplet(s) have been registered.`
+    )
 
     // Set outputs for other workflow steps to use
-    core.setOutput("status", true);
+    // core.setOutput('status', true)
   } catch (error) {
     // Fail the workflow run if an error occurs
-    if (error instanceof Error) core.setFailed(error.message);
+    if (error instanceof Error) core.setFailed(error.message)
   }
 }
